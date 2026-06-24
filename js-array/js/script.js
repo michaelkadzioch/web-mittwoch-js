1c1c9c5fefb79eaf4e314b9534ace787069d0749
Michael Kadzioch <michael@kadzioch.net>
2026-06-24 19:31:47 +0200

    work with array input and output

--- js-array/js/script.js
+++ js-array/js/script.js
@@ -1,11 +1,7 @@
 const button1 = document.getElementById('btn1');
+const eingabe = document.getElementById('input-item');
 
 let koffer = [];
-
-koffer.push('himbeere');
-koffer.push('himbeere');
-koffer.push('himbeere');
-
 
 button1.addEventListener('click', function() {
     kofferPacken();
@@ -15,15 +11,42 @@ button1.addEventListener('click', function() {
 
 
 function kofferPacken() {
-    // hier eingabefeld in kofferarrray puschen
+    // nur einpacken, wenn Feld nicht leer ist
+    if (eingabe.value != '') {
+        koffer.push(eingabe.value);
+        koffer.sort();
+    }
+
+    // Eingabefeld leeren
+    eingabe.value = '';
 }
 
 
 function anzahlImKoffer() {
-    document.getElementById('js-output-anzahl').innerHTML = 'Im Koffer sind ' + koffer.length + ' Gegenstände.';
+    if (koffer.length == 0) {
+        document.getElementById('js-output-anzahl').innerHTML = 'Der Koffer ist leer.';
+    } 
+    else if (koffer.length == 1) {
+        document.getElementById('js-output-anzahl').innerHTML = 'Im Koffer ist nur ein Gegenstand.';
+    }
+    else {
+        document.getElementById('js-output-anzahl').innerHTML = 'Im Koffer sind ' + koffer.length + ' Gegenstände.';
+    }
 }
 
 
 function inhaltImKoffer() {
-    document.getElementById('js-output-inhalt').innerHTML = koffer;
+    let outtext = '';
+
+    if (koffer.length >= 1) {
+        outtext += '<ul>';
+
+        koffer.forEach((gegenstand) => {
+            outtext += '<li>' + gegenstand + '</li>';
+        });
+
+        outtext += '</ul>';
+    }
+
+    document.getElementById('js-output-inhalt').innerHTML = outtext;
 }
